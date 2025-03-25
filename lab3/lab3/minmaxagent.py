6d4bc1641321b37c6cdf545bfd8e96f38aa68530
Filip Pudlak <s198157@student.pg.edu.pl>
2025-03-25 18:21:10 +0100

    Initial commit

---
+++ lab3/lab3/minmaxagent.py
@@ -0,0 +1,70 @@
+import copy
+import time
+class MinMaxAgent:
+    def __init__(self, my_token, depth=4):
+        self.my_token = my_token
+        self.opponent_token = 'o' if my_token == 'x' else 'x'
+        self.depth = depth
+        self.thinking=0
+
+
+    def decide(self, game):
+        start_time = time.time()
+        _, move = self.minimax(game, self.depth, True)
+        end_time = time.time()
+        self.thinking+=end_time-start_time
+        return move
+
+    def minimax(self, game, depth, maximizing_player):
+        if game.game_over or depth == 0:
+            return self.evaluate(game), None
+        
+        valid_moves = game.possible_drops()
+        if maximizing_player:
+            max_eval = float('-inf')
+            best_move = None
+            for move in valid_moves:
+                new_game = copy.deepcopy(game)
+                new_game.drop_token(move)
+                eval_value, _ = self.minimax(new_game, depth - 1, False)
+                if eval_value > max_eval:
+                    max_eval = eval_value
+                    best_move = move
+            return max_eval, best_move
+        else:
+            min_eval = float('inf')
+            best_move = None
+            for move in valid_moves:
+                new_game = copy.deepcopy(game)
+                new_game.drop_token(move)
+                eval_value, _ = self.minimax(new_game, depth - 1, True)
+                if eval_value < min_eval:
+                    min_eval = eval_value
+                    best_move = move
+            return min_eval, best_move
+
+    def evaluate(self, game):
+        if game.wins == self.my_token:
+            return 1000
+        elif game.wins == self.opponent_token:
+            return -1000
+        else:
+            return self.score_position(game)
+
+    def score_position(self, game):
+        score = 0
+        for four in game.iter_fours():
+            score += self.evaluate_four(four)
+        return score
+
+    def evaluate_four(self, four):
+        score = 0
+        if four.count(self.my_token) == 4:
+            score += 100
+        elif four.count(self.my_token) == 3 and four.count('_') == 1:
+            score += 10
+        elif four.count(self.my_token) == 2 and four.count('_') == 2:
+            score += 5
+        if four.count(self.opponent_token) == 3 and four.count('_') == 1:
+            score -= 80
+        return score
